1faafa03de67330f406e1eee39cf28fad0d1e854
Brittney Corona <BrittneyLCorona@gmail.com>
2015-08-28 14:56:22 -0500

    Firebase

---
+++ app/app.js
@@ -0,0 +1,2 @@
+//Make sure to list firebase to make firebase work with the information in songsCtrl.js
+var app = angular.module("PinApp", ['ngRoute', 'firebase']);
---
+++ app/controllers/addCtrl.js
@@ -0,0 +1,21 @@
+//Since this is a controller must list the ref for firebase everytime
+app.controller("PinCtrl", 
+  ["$scope",
+   "$firebaseArray",
+  function($scope, $firebaseArray) {
+
+  	//This will connect to firebase and get the info
+	  var ref = new Firebase("https://pinterest-app.firebaseio.com/addpin");
+	  // download the data into a local object
+	  $scope.songs = $firebaseArray(ref);
+
+	  //Add Song
+		$scope.addPin = function() {
+			$scope.songs.$add({
+				pin_name: $scope.newPin.pin_name,
+				board_type: $scope.newPin.board_type, 
+				description: $scope.newPin.description,
+				image: $scope.newPin.image
+			});
+		};
+}]);
