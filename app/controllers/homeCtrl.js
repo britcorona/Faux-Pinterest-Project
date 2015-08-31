fd54ede9b243d9f61f496a0ea456c2f4585b1939
BrendonPierson <brendonpierson@gmail.com>
2015-08-31 15:45:54 -0500

    added watch functionality for realtime updates

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -3,24 +3,13 @@ app.controller("homeCtrl",
    "$firebaseArray",
    "Auth",
   function($scope, $firebaseArray, Auth) {
-    $scope.name = Auth.$getAuth().facebook.displayName
+    $scope.name = Auth.$getAuth().facebook.displayName;
 
     //This will connect to firebase and get the info
     var ref = new Firebase("https://pinterest-app.firebaseio.com/addpin");
     // download the data into a local object
     $scope.pins = $firebaseArray(ref);
 
-    //Add Pin
-    $scope.addPin = function() {
-      $scope.pins.$add({
-        pin_name: $scope.newPin.pin_name,
-        //board_id: $scope.newPin.board_id, 
-        description: $scope.newPin.description,
-        image: $scope.newPin.image,
-        origUrl: $scope.newPin.origUrl,
-      });
-      $scope.newPin = {"":""};
-    };
-  }]);
+}]);
 
 //current user id, filter by the user id, only display filtered pins
